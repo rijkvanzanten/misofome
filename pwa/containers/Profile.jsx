b25488ef8eef1c7e5cc3a83afeb409ef1d9bf7ec
Rijk van Zanten <rijkvanzanten@me.com>
2017-01-11 23:36:38 +0100

    :sparkles: add profile page, start working on dynamic data

---
+++ pwa/containers/Profile.jsx
@@ -0,0 +1,55 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+
+import IconButton from 'material-ui/IconButton';
+import IconSettings from 'material-ui/svg-icons/action/settings';
+
+import TopBar from '../components/TopBar';
+
+const styles = {
+  header: {
+    position: 'relative',
+    height: '70vw',
+  },
+  avatar: {
+    position: 'absolute',
+    left: 0,
+    top: 0,
+    width: '100%',
+    height: '100%',
+    objectFit: 'cover',
+  },
+  name: {
+    textAlign: 'center',
+    fontWeight: '400',
+    fontSize: '2em',
+    color: 'white',
+    position: 'absolute',
+    left: '20px',
+    bottom: '20px',
+    margin: 0,
+    textShadow: '0 1px 2px rgba(0,0,0,0.5)',
+  },
+};
+
+const mapStateToProps = state => ({ user: state.user });
+
+class Profile extends Component {
+  render() {
+    return (
+      <div>
+        <TopBar
+          title="Profiel"
+          iconElementRight={<IconButton><IconSettings /></IconButton>}
+        />
+        <header style={styles.header}>
+          <img style={styles.avatar} src="http://placehold.it/500x500" />
+          <h2 style={styles.name}>{this.props.user.username}</h2>
+        </header>
+        <main />
+      </div>
+    );
+  }
+}
+
+export default connect(mapStateToProps)(Profile);
--- pwa/routes.jsx
+++ pwa/routes.jsx
@@ -4,11 +4,13 @@ import { Route, IndexRoute } from 'react-router';
 import App from './containers/App';
 import Favorites from './containers/Favorites';
 import Cards from './containers/Cards';
+import Profile from './containers/Profile';
 
 const routes = (
   <Route path="/" component={App}>
     <IndexRoute component={Favorites} />
     <Route path="/kaarten" component={Cards} />
+    <Route path="/profiel" component={Profile} />
   </Route>
 );
 
