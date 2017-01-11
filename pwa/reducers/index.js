bd75d124cbfa5de7c7fce74a67a360b2b303cd00
Rijk van Zanten <rijkvanzanten@me.com>
2017-01-11 20:25:33 +0100

    :fire: wow. its gone. bye now Joking aside, i'm starting over with live content in mind this time

--- pwa/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-
-import user from './user';
-import cards from './cards';
-
-const rootReducer = combineReducers({ user, cards });
-
-export default rootReducer;
