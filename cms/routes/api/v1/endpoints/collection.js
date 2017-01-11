ed7e65b7753d39bca8ada37898c09245088ac0a0
Rijk van Zanten <rijkvanzanten@me.com>
2017-01-11 23:44:35 +0100

    :rotating_light: fix lint errors

--- cms/routes/api/v1/endpoints/collection.js
+++ cms/routes/api/v1/endpoints/collection.js
@@ -1,4 +1,5 @@
 const express = require('express');
+
 const router = express.Router();
 
 const addModelIfExists = require('../middleware/add-model-if-exists');
@@ -9,32 +10,32 @@ router.get('/:model', checkToken, addModelIfExists, (req, res) => {
   const collection = res.model.find({});
 
   // Populate sub-fields with ?populate=[sub],[sub] query
-  if(req.query.populate) {
-    req.query.populate.split(',').forEach(populateOption => {
+  if (req.query.populate) {
+    req.query.populate.split(',').forEach((populateOption) => {
       collection.populate(populateOption);
     });
   }
 
-  // ?where[field]={value} 
-  if(req.query.where) {
-    Object.keys(req.query.where).forEach(key => {
+  // ?where[field]={value}
+  if (req.query.where) {
+    Object.keys(req.query.where).forEach((key) => {
       collection.where(key).equals(req.query.where[key]);
     });
   }
- 
+
   collection.exec((err, records) => {
     res.json(records);
   });
 });
 
 router.post('/:model', checkToken, addModelIfExists, (req, res) => {
-  const doc = new res.model(req.body);
+  const doc = new res.model(req.body); // eslint-disable-line new-cap
 
   doc.save((err, record) => {
     res.json({
       success: true,
       message: '',
-      records: record
+      records: record,
     });
   });
 });
