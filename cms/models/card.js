ed798881714c571b4e7a5a7984f8c94a6f634569
Rijk van Zanten <rijkvanzanten@me.com>
2017-01-11 22:23:32 +0100

    :rotating_light: lint models

--- cms/models/card.js
+++ cms/models/card.js
@@ -6,8 +6,8 @@ const cardSchema = mongoose.Schema({
   category: String,
   user: {
     type: mongoose.Schema.Types.ObjectId,
-    ref: 'user'
-  }
+    ref: 'user',
+  },
 });
 
 const Card = mongoose.model('card', cardSchema);
