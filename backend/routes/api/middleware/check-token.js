6c96e4c457e42cf273199f7d507a1b00dc48f03a
Rijk van Zanten <rijkvanzanten@me.com>
2017-01-20 15:28:12 +0100

    :construction: refactor auth check

--- backend/routes/api/middleware/check-token.js
+++ backend/routes/api/middleware/check-token.js
@@ -1,25 +1,21 @@
 const jwt = require('jsonwebtoken');
-const app = require('../../../../index');
+const app = require('../../../index');
 
 module.exports = (req, res, next) => {
-  const token = req.body.access_token || req.query.access_token || req.headers['x-access-token'];
+  const token = req.headers['x-access-token'];
 
   if (token && token.length > 0) {
     jwt.verify(token, app.get('secretString'), (err, decoded) => {
-      if (err) {
-        res.json({
-          success: false,
-          message: 'Failed to authenticate token.',
-        });
-      } else {
-        req.user = decoded._doc;
-        next();
+      // Something fails in with decoding jwt
+      if(err) {
+        console.log(err);
+        return res.status(500).end();
       }
+
+      req.userID = decoded._id;
+      next();
     });
   } else {
-    res.status(403).json({
-      success: false,
-      message: 'No token provided.',
-    });
+    res.status(401).end();
   }
 };
