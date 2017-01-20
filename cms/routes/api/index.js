a6c56715f182a0dda8f686dcfe9a1310bd03a725
Rijk van Zanten <rijkvanzanten@me.com>
2017-01-20 14:26:13 +0100

    :sparkles: add 404 page

--- cms/routes/api/index.js
+++ cms/routes/api/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 
-router.use('/1', require('./v1'));
+router.use('/user', require('./endpoints/user'));
+router.use('/collection', require('./endpoints/collection'));
+router.use('*', (req, res) => res.status(404).send('Route not found'));
 
 module.exports = router;
